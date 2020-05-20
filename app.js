f7d932d224d37c00b02ac90ac84ca305278a091b
鄭又誠 <zxcy9980@gmail.com>
2020-05-20 17:28:53 +0800

    feat: add mongoose

--- app.js
+++ app.js
@@ -1,10 +1,31 @@
 const express = require('express')
 const app = express()
+const exphbs = require('express-handlebars')
+const mongoose = require('mongoose') // 載入 mongoose
+mongoose.connect('mongodb://localhost/expense-tracker',  { useNewUrlParser: true, useUnifiedTopology: true }) // 設定連線到 mongoDB
+
+// 取得資料庫連線狀態
+const db = mongoose.connection
+// 連線異常
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+// 連線成功
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
+
+
+
+//設定前端模板引擎
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
+app.set('view engine', 'hbs')
 
 app.get('/', (req, res) => {
-    res.send('hello')
+    res.render('index')
 })
 
 app.listen(3000, () => {
     console.log('123')
 })
+
