8bec55423b5e2069b51fb231d16d5af136032763
鄭又誠 <zxcy9980@gmail.com>
2020-06-18 09:41:57 +0800

    新增驗證

--- models/seeds/recordSeeder.js
+++ models/seeds/recordSeeder.js
@@ -1,24 +1,26 @@
 const Record = require('../Record')
 const db = require('../../config/mongoose')
 
+const myList = [
+  { name: '午餐', category: '04', date: '2019/04/23', amount: 60, },
+  { name: '晚餐', category: '04', date: '2019/04/23', amount: 60 },
+  { name: '捷運', category: '02', date: '2019/04/23', amount: 120 },
+  { name: '電影：驚奇隊長', category: '03', date: '2019/04/23', amount: 220 },
+  { name: '租金', category: '05', date: '2019/04/01', amount: 25000 }
+]
 db.once('open', () => {
   console.log('mongodb connected!')
-  const myList = [
-    { name: '午餐', category:'04', date: '2019/04/23', amount: 60, },
-    { name: '晚餐', category:'04',date: '2019/04/23', amount: 60},
-    { name: '捷運', category:'02',date: '2019/04/23', amount: 120},
-    { name: '電影：驚奇隊長',category:'03',date: '2019/04/23', amount: 220},
-    { name: '租金', category:'05',date: '2019/04/01', amount: 25000}
-  ]
-  
-  myList.forEach(list => {
+
+  for (let i = 0; i < myList.length; i++) {
     Record.create({
-      "name": list.name,
-      "category": list.category,
-      "icon":"",
-      "date": list.date,
-      "amount": list.amount,      
+      'name': myList[i].name,
+      'category': myList[i].category,
+      'date': myList[i].date,
+      'amount': myList[i].amount,
+
     })
-  })
+
+  }
   console.log('record done')
 })
+
--- routes/modules/record.js
+++ routes/modules/record.js
@@ -10,18 +10,26 @@ router.get('/new', (req, res) => {
 
 //新增路由
 router.post('/', (req, res) => {
+    if (!isNaN(req.body.amount)) {
+        const { name, date, category, amount } = req.body
+        return Record.create({ name, date, category, amount })     // 存入資料庫
+            .then(() => res.redirect('/')) // 新增完成後導回首頁
+            .catch(error => console.log(error))
+    }
+   else{
     const { name, date, category, amount } = req.body
-    return Record.create({ name, date, category, amount })     // 存入資料庫
-        .then(() => res.redirect('/')) // 新增完成後導回首頁
-        .catch(error => console.log(error))
+     return res.render('new' , { name, date,category, amount })
+   }
 })
 
 //進入ED路由
 router.get('/:id/edit', (req, res) => {
     const id = req.params.id
+    
+    console.log(categoryId)
     return Record.findById(id)
         .lean()
-        .then((record) => res.render('edit', { record }))
+        .then((record) => res.render('edit', { record,}))
         .catch(error => console.log(error))
 })
 
@@ -46,7 +54,7 @@ router.put('/:id', (req, res) => {
 
 //刪除路由
 router.delete('/:id', (req, res) => {
-    const id = req.params.id        
+    const id = req.params.id
     return Record.findById(id)
         .then(record => record.remove())
         .then(() => res.redirect('/'))
