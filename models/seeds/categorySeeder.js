d4baf1cc3cebbd448e0c7738928fa6a406cb3b87
鄭又誠 <zxcy9980@gmail.com>
2020-06-18 18:50:33 +0800

    modify todoSeeder

--- models/seeds/categorySeeder.js
+++ models/seeds/categorySeeder.js
@@ -1,6 +1,11 @@
-const Category = require('../Category')
+const bcrypt = require('bcryptjs')
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
 
+const Category = require('../Category')
 const db = require('../../config/mongoose')
+
 const categoryList = [
   { name: '家居物業',id: '01' , icon:'<i class="fas fa-home"></i>'},
   { name: '交通出行',id: '02', icon:'<i class="fas fa-shuttle-van"></i>'},
@@ -8,16 +13,13 @@ const categoryList = [
   { name: '餐飲食品',id: '04', icon:'<i class="fas fa-utensils"></i>' },
   { name: '其他',id: '05', icon:'<i class="fas fa-pen"></i>'}
 ]
-db.once('open', () => {
-  console.log('mongodb connected!')  
- 
-  for (let i = 0; i < categoryList.length; i++) {
-    Category.create({
-      'name': categoryList[i].name,      
-      'id':categoryList[i].id,
-      'icon':categoryList[i].icon,
-    })
-  }
-
-  console.log('done!')
+db.once('open', () => { 
+  
+    return Promise.all(Array.from(
+      { length: 5 },
+      (_, i) => Category.create({
+         name: categoryList[i].name, id: categoryList[i].id, icon: categoryList[i].icon, })
+    )) 
+  console.log('finish')
+    
 })
--- models/seeds/recordSeeder.js
+++ models/seeds/recordSeeder.js
@@ -1,28 +1,47 @@
+const bcrypt = require('bcryptjs')
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+const User = require('../user')
 const Record = require('../Record')
 const db = require('../../config/mongoose')
 
+const SEED_USER = {
+  name: 'root',
+  email: 'root@example.com',
+  password: '12345678'
+}
+
 const myList = [
-  { name: '午餐', category: '04', categoryname: '餐飲食品', date: '2019/04/23',month:'4', shop:'麥當勞', amount: 60, },
-  { name: '晚餐', category: '04', categoryname: '餐飲食品', date: '2019/04/23',month:'4', shop:'吉野家', amount: 60 },
-  { name: '捷運', category: '02', categoryname: '交通出行',date: '2019/04/23',month:'4', shop:'台北捷運', amount: 120 },
-  { name: '電影：驚奇隊長', category: '03', categoryname: '休閒娛樂', date: '2019/04/23', month:'4', shop:'威秀影城', amount: 220 },
-  { name: '租金', category: '05', categoryname: '其他',date: '2019/04/01',month:'4', shop:'房東', amount: 25000 }
+  { name: '午餐', category: '04', categoryname: '餐飲食品', date: '2019/04/23', month: '4', shop: '麥當勞', amount: 60, },
+  { name: '晚餐', category: '04', categoryname: '餐飲食品', date: '2019/04/23', month: '4', shop: '吉野家', amount: 60 },
+  { name: '捷運', category: '02', categoryname: '交通出行', date: '2019/04/23', month: '4', shop: '台北捷運', amount: 120 },
+  { name: '電影：驚奇隊長', category: '03', categoryname: '休閒娛樂', date: '2019/04/23', month: '4', shop: '威秀影城', amount: 220 },
+  { name: '租金', category: '05', categoryname: '其他', date: '2019/04/01', month: '4', shop: '房東', amount: 25000 }
 ]
+
 db.once('open', () => {
-  console.log('mongodb connected!')
+  bcrypt
+    .genSalt(10)
+    .then(salt => bcrypt.hash(SEED_USER.password, salt))
+    .then(hash => User.create({
+      name: SEED_USER.name,
+      email: SEED_USER.email,
+      password: hash
+    }))
 
-  for (let i = 0; i < myList.length; i++) {
-    Record.create({
-      'name': myList[i].name,
-      'category': myList[i].category,
-      'categoryname': myList[i].categoryname,
-      'date': myList[i].date,
-      'amount': myList[i].amount,
-      'month':myList[i].month,
-      'shop':myList[i].shop,
+    .then(user => {      
+      const userId = user._id
+      return Promise.all(Array.from(
+        { length: 5 },
+        (_, i) => Record.create({
+           name: myList[i].name, category: myList[i].category, categoryname: myList[i].categoryname, date: myList[i].date,amount: myList[i].amount, month: myList[i].month, shop: myList[i].shop, userId 
+          })
+      ))
     })
+    .then(() => {
+      console.log('done')
+      process.exit()
+    })   
 
-  }
-  console.log('record done')
-})
-
+  })
