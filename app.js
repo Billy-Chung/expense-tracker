3e1e18c1f5b8907826c2a904b143f42b631bf446
鄭又誠 <zxcy9980@gmail.com>
2020-06-18 16:17:23 +0800

    feat: add user login feature via passport

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')//載入express並架設伺服器
 const app = express()
 const session = require('express-session')
+const usePassport = require('./config/passport')
 const exphbs = require('express-handlebars')
 const Record = require('./models/Record')//載入資料
 const Category = require('./models/Category')//載入資料
@@ -16,6 +17,8 @@ app.use(session({
   resave: false,
   saveUninitialized: true
 }))
+
+usePassport(app)
 
 app.use(bodyParser.urlencoded({ extended: true }), methodOverride('_method'))// 用 app.use 規定每一筆請求都需要透過 body-parser 進行前置處理
 app.use(routes)// 將 request 導入路由器
--- routes/modules/users.js
+++ routes/modules/users.js
@@ -1,10 +1,20 @@
 const express = require('express')
 const router = express.Router()
 const User = require('../../models/user')
+const passport = require('passport')
 
 
 router.get('/login', (req, res) => {
   res.render('login')
+})
+
+router.post('/login', passport.authenticate('local', {
+  successRedirect: '/',
+  failureRedirect: 'users/login'
+}))
+
+router.get('/register', (req, res) => {
+  res.render('register')
 })
 
 router.post('/register', (req, res) => {
@@ -34,9 +44,7 @@ router.post('/register', (req, res) => {
   })
 })
 
-router.get('/register', (req, res) => {
-  res.render('register')
-})
+
 
 
 module.exports = router
